13113280a2087424423d6065e81716350c817f7e
Idogwu Chinonso <idogwuchinonso1@gmail.com>
2025-06-16 17:47:52 +0100

    mod: add resend otp route

--- src/config/config_scope.rs
+++ src/config/config_scope.rs
@@ -1,9 +1,9 @@
 use crate::routes::healthz::check_health;
 use crate::routes::users::profile::{
     create_user_handler, get_user_bank_accounts_handler, get_user_handler,
-    register_user_bank_account_handler,
+    register_user_bank_account_handler, resend_otp_handler,
 };
-use actix_web::web;
+use actix_web::web::{self, service};
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api/v1")
@@ -11,6 +11,7 @@ pub fn config(conf: &mut web::ServiceConfig) {
         .service(create_user_handler)
         .service(register_user_bank_account_handler)
         .service(get_user_bank_accounts_handler)
-        .service(get_user_handler);
+        .service(get_user_handler)
+        .service(resend_otp_handler);
     conf.service(scope);
 }
